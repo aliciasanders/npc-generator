dd00a449544e0b0543da6546035886f167eabdec
Alicia Sanders <alicia@aliciasanders.dev>
2019-08-28 17:33:41 -0400

    Implement load character ajax script

---
+++ loadCharacter.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once(Character.class.php);
+require_once(DBManager.class.php);
+
+try {
+    $character = Character::loadCharacter($_POST['characterID']);
+
+    $returnArray = array(
+        "success" => true,
+        "error" => false,
+        "character" => json_encode($character)
+    );
+} catch (Exception $e) {
+    $returnArray array(
+        "success" => false,
+        "error" => "There was an error loading your character, please try again later.",
+        "character" => false
+    );
+}
+
+echo json_encode($returnArray);
