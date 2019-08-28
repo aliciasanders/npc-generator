aaca6cbdb43d1c5dcfcd04e8b5a177902f339259
Alicia Sanders <alicia@aliciasanders.dev>
2019-08-28 16:59:25 -0400

    Implement generate character for ajax request

---
+++ generateCharacter.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once(Character.class.php);
+require_once(DBManager.class.php);
+
+try {
+    echo json_encode( Character::randomCharacter() );
+} catch (Exception $e) {
+    echo "\0";
+}
