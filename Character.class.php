393215925d5a5ce70b5705b95f0f90ccca45a1b6
Alicia Sanders <alicia@aliciasanders.dev>
2019-09-03 15:45:21 -0400

    Character class fixes - misc typost and add getJSON method

--- Character.class.php
+++ Character.class.php
@@ -21,17 +21,18 @@ class Character {
      * Accepts array of character data
      */
     function __construct( $characterData ) {
+
+        $this->dbManager = new DBManager();
+
         $this->id = isset($characterData['id']) ? $characterData['id'] : uniqid();
 
         $this->setName( isset($characterData['name']) ? $characterData['name'] : null );
         $this->setRace( isset($characterData['race']) ? $characterData['race'] : null );
         $this->setGender( isset($characterData['gender']) ? $characterData['gender'] : null );
         $this->setAge( isset($characterData['age']) ? $characterData['age'] : null );
-        $this->setPhysical_traits( isset($characterData['physical_traits']) ? $characterData['physical_traits'] : null );
-        $this->setPersonality_traits( isset($characterData['personality_traits']) ? $characterData['personality_traits'] : null );
+        $this->setPhysicalTraits( isset($characterData['physical_traits']) ? $characterData['physical_traits'] : null );
+        $this->setPersonalityTraits( isset($characterData['personality_traits']) ? $characterData['personality_traits'] : null );
         $this->setGoals( isset($characterData['goals']) ? $characterData['goals'] : null );
-
-        $this->dbManager = new DBManager();
     }
     
     /**
@@ -56,11 +57,29 @@ class Character {
     public function getPhysicalTraits(){
         return $this->physical_traits;
     }
-    public function getPersonalityTtraits(){
+    public function getPersonalityTraits(){
         return $this->personality_traits;
     }
     public function getGoals(){
         return $this->goals;
+    }
+
+    /**
+     * function to serialize character data to json
+     */
+    public function getJSON() {
+        return json_encode( array(
+            
+            'id' => $this->id,
+            'name' => $this->name,
+            'race' => $this->race,
+            'gender' => $this->gender,
+            'age' => $this->age,
+            'physical_traits' => $this->physical_traits,
+            'personality_traits' => $this->personality_traits,
+            'goals' => $this->goals,
+
+        ));
     }
 
     /**
@@ -71,7 +90,7 @@ class Character {
         $this->name = $name;
     }
     public function setRace( $race ){
-        $raceOptions = $dbManager->getOptions('race');
+        $raceOptions = $this->dbManager->getOptions('race');
         if ( in_array( $race, $raceOptions ) ) {
             $this->race = $race;
         } else {
@@ -79,7 +98,7 @@ class Character {
         }
     }
     public function setGender( $gender ){
-        $genderOptions = $dbManager->getOptions('gender');
+        $genderOptions = $this->dbManager->getOptions('gender');
         if ( in_array( $gender, $genderOptions ) ) {
             $this->gender = $gender;
         } else {
@@ -92,7 +111,7 @@ class Character {
     public function setPhysicalTraits( $physical_traits ){
         $this->physical_traits = $physical_traits;
     }
-    public function setPersonalityTtraits( $personality_traits ){
+    public function setPersonalityTraits( $personality_traits ){
         $this->personality_traits = $personality_traits;
     }
     public function setGoals( $goals ){
@@ -105,7 +124,7 @@ class Character {
     public function save() {
         session_start();
         $characters = isset($_SESSION['characters']) ? $_SESSION['characters'] : array();
-        $characters[$this->id] = json_encode($this);
+        $characters[$this->id] = $this->getJSON();
         $_SESSION['characters'] = $characters;
     }
 
@@ -114,7 +133,7 @@ class Character {
      * JSON should be an array with keys corresponding to the Character attributes
      */
     public static function loadJSON( $jsonString ) {
-        $characterData = json_decode( $jsonString );
+        $characterData = json_decode( $jsonString, true );
         return new Character($characterData);
     }
 
@@ -140,7 +159,9 @@ class Character {
         $characters = array();
         if ( isset( $_SESSION['characters'] ) ) {
             foreach( $_SESSION['characters'] as $characterID => $characterData ) {
-                $characters['$characterID'] = json_decode($characterData)['name'];
+                if (isset( json_decode($characterData, true)['name'] )) {
+                    $characters["$characterID"] = json_decode($characterData, true)['name'];
+                }
             }
         }
         return $characters;
@@ -155,7 +176,7 @@ class Character {
         $attributes['name'] = $dbManager->randomOption('name');
         $attributes['race'] = $dbManager->randomOption('race');
         $attributes['gender'] = $dbManager->randomOption('gender');
-        $attributes['age'] = $dbManager->randomOption('age');
+        $attributes['age'] = rand( 18, 70 );
         $attributes['physical_traits'] = $dbManager->randomOption('physical_traits');
         $attributes['personality_traits'] = $dbManager->randomOption('personality_traits');
         $attributes['goals'] = $dbManager->randomOption('goals');
