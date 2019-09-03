c839789d8de1d43c02a5def5a949235789b97ddb
Alicia Sanders <alicia@aliciasanders.dev>
2019-09-03 15:47:39 -0400

    implement javascript functions for initial page load, as well as character generating, loading, and saving

---
+++ scripts.js
@@ -0,0 +1,124 @@
+$(document).ready(function() {
+
+    /**
+     * Initial Setup
+     */
+     
+    // if there is at least one existing character option (other than 'new character') load the first character option
+    if ($('#characterSelectField option').length > 2) {
+        var character = $('#characterSelectField option:nth-child(2)').val();
+        $('#characterSelectField').val(character);
+        loadCharacter();
+    } 
+    // Otherwise create a new generated character
+    else {
+        newCharacter();
+    }
+
+    // Attach function triggers
+    $('.loadCharacter').click( loadCharacter );
+    $('.newCharacter').click( newCharacter );
+    $('.saveCharacter').click( saveCharacter );
+
+});
+
+/**
+ * Load a saved character
+ */
+function loadCharacter() {
+
+    var ID = $('#characterSelectField').val();
+    if ( ID !== '0' ) {  // if there is a character currently selected
+
+        // Load the selected character and display the character's data
+        $.post( '/loadCharacter.php',
+        {characterID: ID},
+        function( dataString ) {
+            var data = JSON.parse(dataString);
+            if (data.success) {
+                var characterData = JSON.parse(data.character);
+                displayCharacterData( characterData );
+            } else {
+            }
+        });
+
+    }
+
+
+}
+
+/**
+ * Load a new character and display in form options
+ */
+function newCharacter() {
+    $.post( '/newCharacter.php',
+    {},
+    function( dataString ) {
+        data = JSON.parse(dataString);
+        if (data.success) {
+            $('#characterSelectField').val(0);
+            displayCharacterData( JSON.parse( data.character ) );
+        } else {
+
+        }
+    });
+}
+
+/**
+ * Save the current character
+ */
+function saveCharacter () {
+
+    characterDataObject = {};
+
+    // Name
+    characterDataObject.name = $('#nameField').val();
+    // Race
+    characterDataObject.race = $('#raceField').val();
+    // Age
+    characterDataObject.age = $('#ageField').val();
+    // Gender
+    characterDataObject.gender = $('#genderField').val();
+    // Physical Traits
+    characterDataObject.physical_traits = $('#physicalTraitsField').val();
+    // Personality Traits
+    characterDataObject.personality_traits = $('#personalityTraitsField').val();
+    // Goals
+    characterDataObject.goals = $('#goalsField').val();
+
+    $.post( '/saveCharacter.php',
+    {characterData: JSON.stringify(characterDataObject)},
+    function( dataString ) {
+        data = JSON.parse(dataString);
+        if (data.success) {
+            var characterData = JSON.parse(data.character);
+            $('#characterSelectField').append('<option value="'+characterData.id+'">'+characterData.name+'</option>');
+            $('#characterSelectField').val(characterData.id);
+            displayCharacterData( characterData );
+        } else {
+        }
+    });
+
+}
+
+/**
+ * Display a character in form options
+ */
+function displayCharacterData( characterData ) {
+
+    // Name
+    $('#nameField').val(characterData.name);
+    // Race
+    $('#raceField').val(characterData.race);
+    // Age
+    $('#ageField').val(characterData.age);
+    // Gender
+    $('#genderField').val(characterData.gender);
+    // Physical Traits
+    $('#physicalTraitsField').val(characterData.physical_traits);
+    // Personality Traits
+    $('#personalityTraitsField').val(characterData.personality_traits);
+    // Goals
+    $('#goalsField').val(characterData.goals);
+
+}
